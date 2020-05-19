775c2238026b7ab49bbc66b9feffb5f00924c93e
Kareem Zakaria <kareemahmed4996@gmail.com>
2020-05-19 13:40:58 +0000

    Watson Author Finder - Initial Code

---
+++ app.js
@@ -0,0 +1,23 @@
+var port = process.env.VCAP_APP_PORT || 8080;
+
+//Express Web Framework, and create a new express server
+let express = require('express');
+let bodyParser = require('body-parser');
+
+//Routes modules
+let index = require('./routes/index'),
+author = require('./routes/author');
+
+let app = express();
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//In case the caller access any URI under the root /, call index route
+app.use('/', index);
+
+//In case the caller access any URI under /author, call author route
+app.use('/author', author);
+
+// start server on the specified port and binding host
+app.listen(port);
